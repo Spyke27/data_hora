df830958e49688f70b489ceb4bb656a23f963063
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-06 16:25:43 -0300

    new Date

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,22 +1,27 @@
 package app;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Program {
 
 	public static void main(String[] args) {
-		//DateTimeFormatter br = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-		LocalDate d01 = LocalDate.now();
+		Date date = new Date();
+		SimpleDateFormat brDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		System.out.println(brDateFormat.format(date));
+
+		/* LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
 		Instant d03 = Instant.now();
 
 		System.out.println("Data: "+d01);
 		System.out.println("Data e hora: "+d02);
-		System.out.println("Instante atual em Londres: "+d03);
+		System.out.println("Instante atual em Londres: "+d03); */
 		
 	}
 
